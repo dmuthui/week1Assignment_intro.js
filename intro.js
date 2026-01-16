184bd13a859352f468f231390128e6d6d0c0f4ea
dmuthui <vyda2002us@gmail.com>
2026-01-16 19:39:43 +0300

    Initial commit: Week1 Assignment - intro.js

---
+++ intro.js
@@ -0,0 +1,10 @@
+const developer = {
+    name: "David Muthui Ndung'u",
+    age: 43,
+    skill: "JavaScript"
+};
+
+console.log(`🚀 Hello Technified Team!
+My name is ${developer.name}.
+I am ${developer.age} years old, and I❤️  JavaScript ${developer.skill}.
+Let's build something great!`);
